210b7c7ab33fb728a415c26a9226c2222d56e0ad
tshabc <tshabc87@163.com>
2016-01-14 23:08:38 +0800

    快速排序算法代码实现，添加交换两个数值的公共函数

--- 经典六大排序算法/经典六大排序算法/Utils.h
+++ 经典六大排序算法/经典六大排序算法/Utils.h
@@ -1,6 +1,7 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 #include <iostream>
+using namespace std;
 static void coutArray(int *array, int length)
 {
 	if (array == NULL)
@@ -12,5 +13,29 @@ static void coutArray(int *array, int length)
 		std::cout << "array[i] = " << array[i] << std::endl;
 	}
 }
+/* 
+	交换数组中的两个数
+	@param:	array 一个 int 型 数组
+	@param:	i 一个下标
+	@param:	j 一个下标
+*/
+static void swapNum(int * array,int i,int j)
+{
+	if (array == NULL)
+	{
+		std::cout << "param error " << __FILE__ << __FUNCTION__ << __LINE__ << std::endl;
+		return;
+	}
+	cout << "array[i] = " << array[i] << "array[j] = " << array[j] << endl;
+/*	如果两个数相等不能用这种方法
+	array[i] = array[i] + array[j];
+	array[j] = array[i] - array[j];
+	array[i] = array[i] - array[j];*/
+	int temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
+	cout << "array[i] = " << array[i] << "array[j] = " << array[j] << endl;
+}
+
 
 #endif // !_UTILS_H_
